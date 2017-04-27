61f88f9b8fc3b14c128b11890ea879f761d86971
S820108S <B10023031@yuntech.org.tw>
2017-04-27 21:56:08 +0800

    OK

---
+++ leadCode.service/soluation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leadCode.service
+{
+    public class soluation
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Hashtable ht = new Hashtable();
+            List<int> list = new List<int>();
+            list = nums.ToList<int>();
+            int i = 0;
+            foreach (int num in list)
+            {
+                if (!ht.Contains(num)) ht.Add(num, i);
+                i++;
+            }
+            i = 0;
+            foreach (int num in list)
+            {
+                int temp = target - num;
+                if (ht.Contains(temp) && i != (int)ht[temp])
+                {
+                    int[] ans = new int[2];
+                    ans[0] = i;
+                    ans[1] = (int)ht[temp];
+                    return ans;
+                }
+                i++;
+            }
+            return null;
+        }
+    }
+}
---
+++ leadCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leadCode.service;
+
+namespace leadCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            soluation service = new soluation();
+
+            int[] ans = new int[2];
+            int[] nums = new int[] { 3,2, 4};
+            int target = 6;
+
+            ans = service.TwoSum(nums, target);
+
+            for (int i = 0; i < ans.Length; i++)
+            {
+                Console.Write(ans[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
